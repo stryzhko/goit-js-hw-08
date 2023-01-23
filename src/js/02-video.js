f5ca4b80ba889d1e8a93148a9b5fc42a8d7750e0
stryzhko <stryzhko@gmail.com>
2023-01-23 16:28:41 +0200

    Add task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
 
+const throttle = require('lodash.throttle');
+
+const ifFrameRef = document.querySelector('#vimeo-player');
+const player = new Player(ifFrameRef);
+
+player.on('timeupdate', throttle(setWatchingTime, 1000));
+
+function setWatchingTime(e) {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+}
+
+const getTimeFromLocalStorage = (
+  localStorage.getItem('videoplayer-current-time') || 0
+);
+
+player.setCurrentTime(getTimeFromLocalStorage);
